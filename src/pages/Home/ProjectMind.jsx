e1f482c6c1b31e5dfaf5732b0e1fe2f1e5264310
trusha97 <thummartrusha97@gmail.com>
2025-07-25 11:24:33 +0530

    add new error solve

---
+++ src/pages/Home/ProjectMind.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './ProjectMind.css';
+import { projectmindimg } from '../../Utils/images';
+
+const ProjectMind = () => {
+  return (
+    <div className='project-mind-div-main'>
+        <p className="top-text">HAVE YOU PROJECT IN MIND?</p>
+    <div className="project-mind-main">
+      <img src={projectmindimg} alt="Blueprint" className="blueprint-image" />
+
+      <div className="overlay">
+        <h1 className="heading">
+          Create Your Creative <br />
+          Unique Design
+        </h1>
+        <button className="contact-button">
+          Contact Design <span className="arrow">→</span>
+        </button>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default ProjectMind;
