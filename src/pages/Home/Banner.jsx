bdc4b921b1db6c9d53c72e55c3ef62eccee384f9
trusha97 <thummartrusha97@gmail.com>
2025-07-25 17:36:59 +0530

    banner section start

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import './Banner.css';
+import heroImage from '../../assets/images/hero.webp';
+import { gaimg } from '../../utils/images';
+
+const Banner = () => {
+    return (
+        <div
+            className="banner-main"
+            style={{ backgroundImage: `url(${heroImage})` }}
+        >
+            <div className="banner-overlay">
+                <h1 className="banner-title">
+                    You Approach The Architecture
+                </h1>
+
+                <div className="box box-right">
+                    <div className="circle-img" />
+                    <div>
+                        <p className="box-title"> Our company Architect  Firm</p>
+                        <p className="box-sub">Watch the videos (2.40s)</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='banner-div-image-main'>
+                <div className='banner-round-main-div'>
+                    <div className='banner-div-image-inner '>
+                        <div className='banner-img-circle-div'>
+                            <img src={gaimg} alt="GA Logo" className='ga-logo' />
+                            <svg className="circle-svg" viewBox="0 0 300 300">
+                                <defs>
+                                    <path
+                                        id="circlePath"
+                                        d="M150,150 m-100,0 a100,100 0 1,1 200,0 a100,100 0 1,1 -200,0"
+                                    />
+                                </defs>
+                                <text fill="#fff">
+                                    <textPath href="#circlePath" startOffset="0%">
+                                        GACTUT ARCHITECTURE 2024 MINIMALISTIC DESIGN FOR •
+                                    </textPath>
+                                </text>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="box1 box-right">
+                    <div className="circle-img1" />
+                    <div className='banner-text-des'>
+                        <p className="box-title1">Let’s Build Something Great  Together</p>
+                    </div>
+                </div>
+
+
+            </div>
+            <div className='year-main-div'>
+                <div className='year-container'>
+                    <div className='yeartwofive'>
+                        <h3 className='year-inner-div'>2005</h3>
+                    </div>
+                    <div className='yeartwofive'>
+                        <h3 className='year-inner-div'>2005</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div className="banner-container">
+                <h1 className="banner-overlay-text">Architecture</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/utils/images.jsx
+++ src/utils/images.jsx
@@ -1,3 +1,5 @@
+
+import banner from "../assets/images/hero.webp";
 import hero_section_bg from "../assets/images/hero.webp";
 import facebook from "../assets/images/facebook.png";
 import instagram from "../assets/images/instagram.png";
@@ -17,10 +19,18 @@ import Architectural from "../assets/images/ser1.png";
 import Landscape from "../assets/images/ser2.png";
 import Architect from "../assets/images/ser3.png";
 import quite from "../assets/images/quite.png";
-import projectmindimg from "../assets/images/projectmindbg.webp"
+import projectmindimg from "../assets/images/projectmindbg.webp";
+import decorasion from "../assets/images/imgsection-1.webp";
+import teresh from "../assets/images/imgsection-2.webp";
+import ground from "../assets/images/imgsection-3.webp";
+import dom from "../assets/images/imgsection-4.webp";
+import img_insta from "../assets/images/image-instagram.png";
+import gaimg from "../assets/images/GA.png";
 
 
 
+
+export {banner}
 export {hero_section_bg}
 export {facebook}
 export {instagram}
@@ -41,3 +51,9 @@ export {Landscape}
 export {Architect}
 export {quite}
 export {projectmindimg}
+export {decorasion}
+export {teresh}
+export {ground}
+export {dom}
+export {img_insta}
+export {gaimg}
